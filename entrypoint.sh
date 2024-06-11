7e690a409fa30c82dbcbdfdcf448adf45c1478db
jscblack <33062157+jscblack@users.noreply.github.com>
2024-06-11 15:59:40 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Function to generate random password
+generate_random_password() {
+    openssl rand -base64 6
+}
+
+# Check if password file exists
+if ! [ -f "/root/.ssh/.container_init.pwd" ]; then
+    # If password file not exists
+    
+    # Generate a random password
+    password=$(generate_random_password)
+    
+    # Set the generated password for root user
+    echo "${password}" > /root/.ssh/.container_init.pwd
+
+    # Set the password for root user
+    echo "root:${password}" | chpasswd
+    
+    # Output the password (optional)
+    echo "!!!WARNING: Change the password immediately after logging into the dev container for security reasons!!!"
+    echo "root password: ${password}"
+fi
+
+# Start the SSH server
+/usr/sbin/sshd -D
